dbc888e5ad3ee4e881d7dfb390a18ed1168f1e6e
GodSci3nc3 <rosalesvelazquezarturo@gmail.com>
2025-06-23 20:56:08 -0600

    Empezando con el desarrollo de las api's

---
+++ apis/connection.php
@@ -0,0 +1,10 @@
+<?php
+ 
+        $user = "u538391641_CANT_DA";
+        $pass = "Garciad11?";
+        $server = "localhost";
+        $db ="u538391641_CANT_ACTIV";
+        $con = mysqli_connect($server, $user, $pass, $db);
+
+
+?>
